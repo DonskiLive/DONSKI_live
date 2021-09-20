b96ff62813cdd6d6cd04ef36cbe13af39959ad7d
DonskiLive <vilchynskyy.a@gmail.com>
2021-09-20 22:09:13 +0200

    updated 20.09.2021

---
+++ reactProjects/SaveComment/reactProjectAdvanced/src/context/app-context.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const AppContext = React.createContext();
+/* 
+function mult(x){
+    return (y) =>{
+        return x * y
+    }
+}
+
+const multiplay2 = mult(2); 
+console.log(multiplay2(4))
+console.log(multiplay2(6))
+
+const multyplay3 = mult(3)  
+
+
+function greaterThan(n){
+    return function(m){
+        return m > n
+    }
+}
+
+const greaterThan10 = greaterThan(10);
+console.log(greaterThan10(12))
+console.log(greaterThan10(8))
+ */
