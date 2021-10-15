c06e98a95d81201565abd7ad590e7d5670de5fa0
DonskiLive <vilchynskyy.a@gmail.com>
2021-10-15 14:49:58 +0200

    updated 15.10.2021

--- reactProjects/FindBook/src/config/firebase-config.js
+++ reactProjects/FindBook/src/config/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBNzIFwEv-bKSte4OVJwwM_Ka4h4VqO-BQ",
-	authDomain: "booksdb-a718a.firebaseapp.com",
-	projectId: "booksdb-a718a",
-	storageBucket: "booksdb-a718a.appspot.com",
-	messagingSenderId: "803811107731",
-	appId: "1:803811107731:web:69c8a3b903edba52cffe66"
+	apiKey: '',
+	authDomain: '',
+	projectId: '',
+	storageBucket: '',
+	messagingSenderId: '',
+	appId: ''
 };
 
 export const fb = initializeApp(firebaseConfig);
